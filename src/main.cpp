d2e49def6c46c5b645776b579a218496a970cde1
sword <sword-k@sjtu.edu.cn>
2024-11-24 15:44:12 +0800

    cancel the file operaton to test submission to github

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,10 @@ using namespace antlr4;
 //       if you really need to regenerate,please ask TA for help.
 int main(int argc, const char *argv[]) {
 	// TODO: please don't modify the code below the construction of ifs if you want to use visitor mode
+#ifdef sword
 	freopen("data.txt", "r", stdin);
 	freopen("out.txt", "w", stdout);
+#endif
 	ANTLRInputStream input(std::cin);
 	Python3Lexer lexer(&input);
 	CommonTokenStream tokens(&lexer);
